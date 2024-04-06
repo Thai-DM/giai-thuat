594e3cb72160feded5617b68fd05e24e661b519b
Thai-DM <165550663+Thai-DM@users.noreply.github.com>
2024-04-06 22:08:44 +0700

    Add files via upload

--- BCATM-ATM.cpp
+++ BCATM-ATM.cpp
@@ -1,36 +1,62 @@
 #include <iostream>
 using namespace std;
-void sapxep(int n,int a[100]){
-    for(int i=0;i<n - 1;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i]<a[j]){
-                int t=a[i];
-                a[i]=a[j];
-                a[j]=t;
-            }
-        }
-    }
-}
-int Try1(int n,int s,int a[100]){
-    int d=0;
-    for(int i=0;i<n;i++){
-        d=d+s/a[i];
-        s=s-(s/a[i])*a[i];
-    }
-    if(s==0){
-         return d;
-    }
-    else return -1;
-}
-int main()
+ 
+int n, k;
+int arr[20];
+void read ()
 {
-    int n,s;
-    int a[n];
-    cin>>n>>s;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    sapxep(n,a);
-    cout<<Try1(n,s,a);
-    return 0;
+	cin>>n>>k;
+	arr[0]=0;
+	for (int i=1; i<=n; i++)
+	{
+		int tmp;
+		cin>>tmp;
+		arr[i]=arr[i-1]+tmp;
+	}
+}
+ 
+int count=0;
+int tim (int x, int bg, int ed, int K)
+{
+	if (bg > ed) return 0;
+	if (K==1)
+	{
+		if (x==arr[ed]-arr[bg-1])
+		{
+			count++;
+			return 1;
+		}
+		else return 0;
+	}
+	else if (K==0) return 0;
+	else
+	{
+		int BG = bg;
+		for (int i=bg; i<=ed; i++)
+		{
+			if (arr[i]-arr[BG-1]==x)
+			{
+				tim (x, i+1, ed, K-1);
+			}
+		}
+	}
+}
+ 
+int main ()
+{
+	read ();
+	if (k==1)
+	{
+		count=1;
+	}
+	else
+	{
+		for (int i=1; i<=n; i++)
+		{
+			int label = arr[i];
+			tim (label, i+1, n, k-1);
+		}
+	}
+	cout<<count;
+	return 0;
 }
